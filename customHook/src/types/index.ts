89489080c0f42da3b8cc666f790279d656c1ebce
Fabiola <fabiola.aurelien@gmail.com>
2026-05-02 10:48:38 -0400

    Add interface usePaginationReturnProps to index.ts

--- customHook/src/types/index.ts
+++ customHook/src/types/index.ts
@@ -1,6 +1,20 @@
 
-export interface usePaginationProps{
+export interface usePaginationProps {
     totalItems: string,
     itemsPerPage: number,
     initialPage: number
+}
+
+export interface usePaginationReturnProps {
+    currentPage: number,
+    totalPages: number,
+    startIndex: number,
+    endIndex: number,
+    itemsOnCurrentPage: number,
+    setPage(pageNumber): void,
+    nextPage(): void,
+    prevPage(): void,
+    canNextPage: boolean,
+    canPrevPage: boolean,
+
 }
