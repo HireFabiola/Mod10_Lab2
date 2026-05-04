d4a848a1b0d919c6677af7c531849406ef0a2d0f
Fabiola <fabiola.aurelien@gmail.com>
2026-05-03 22:13:32 -0400

    Wrote useDebounce logic but not sure if working. Need to see how to test it

--- customHook/src/components/useDebounce.ts
+++ customHook/src/components/useDebounce.ts
@@ -3,5 +3,26 @@ import DebounceSearchDemo from "./DebounceSearchDemo";
 
 
 export function useDebounce(value, delay) {
+
+  // Inittialize debouncedValue to input string
   const [debouncedValue, setDebouncedValue] = useState(value);
+
+  //Trigger timeout function with changes in value or delay
+  useEffect(() => {
+
+    // Set timer in handler
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    // Clean up timer everytime timer useDebounced is called
+    return () => {
+      clearTimeout(handler);
+    }
+
+  }, [value, delay]
+  )
+
+  // Return value once timer has expired
+  return debouncedValue;
 }
